7a53a0e921c8cf133e0cfa29d6bb2f45f9070295
Igor Barbashov <igorbarbashov@yandex.ru>
2022-06-25 23:41:57 +0300

    feat: init project

---
+++ src/main/java/ru/yandex/backend/school2/megamarket/MegamarketApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.backend.school2.megamarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegamarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MegamarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/yandex/backend/school2/megamarket/MegamarketApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.backend.school2.megamarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegamarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
