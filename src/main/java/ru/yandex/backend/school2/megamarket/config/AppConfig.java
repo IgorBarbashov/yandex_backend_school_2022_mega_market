aac8fdc0f008c306c9d5e705f00e59097fd4fa33
Igor Barbashov <igorbarbashov@yandex.ru>
2022-06-26 08:27:43 +0300

    feat: add DateTime format on the application level

---
+++ src/main/java/ru/yandex/backend/school2/megamarket/config/AppConfig.java
@@ -0,0 +1,29 @@
+package ru.yandex.backend.school2.megamarket.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
+import org.springframework.format.support.DefaultFormattingConversionService;
+import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+@EnableWebMvc
+public class AppConfig extends WebMvcConfigurationSupport {
+
+    @Bean
+    @Override
+    public FormattingConversionService mvcConversionService() {
+        DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService(false);
+
+        DateTimeFormatterRegistrar dateTimeRegistrar = new DateTimeFormatterRegistrar();
+        dateTimeRegistrar.setDateTimeFormatter(DateTimeFormatter.ISO_LOCAL_DATE);
+        dateTimeRegistrar.registerFormatters(conversionService);
+
+        return conversionService;
+    }
+
+}
