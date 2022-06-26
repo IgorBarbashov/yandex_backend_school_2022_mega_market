5bbb1048c55d3266a734b14d2724bac49fe4c850
Igor Barbashov <igorbarbashov@yandex.ru>
2022-06-26 08:59:34 +0300

    feat: save imports to DB

--- src/main/java/ru/yandex/backend/school2/megamarket/repository/ShopUnitRepository.java
+++ src/main/java/ru/yandex/backend/school2/megamarket/repository/ShopUnitRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.yandex.backend.school2.megamarket.entity.ShopUnit;
 
+import java.util.List;
+
 @Repository
 public interface ShopUnitRepository extends JpaRepository<ShopUnit, String> {
+
+    List<ShopUnit> findByIdIn(List<String> ids);
+
 }
